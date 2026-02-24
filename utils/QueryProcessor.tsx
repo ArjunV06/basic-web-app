ec47d94fa1a49f8a6874b564a0c70a6ab57eaf33
Arjun Vasudevan <arjunvsudn@gmail.com>
2026-02-24 14:49:27 +0000

    done

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Arjun";
+    return "arjunvas";
   }
 
   if (query.toLowerCase().includes("andrew id")) {
